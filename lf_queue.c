7da751c8a3710ab88b787cc05db58222646311a2
TortaXx <tortaxxcz@gmail.com>
2024-05-16 21:05:50 +0200

    Make lf_queue_t singly linked list

--- lf_queue.c
+++ lf_queue.c
@@ -10,7 +10,6 @@
 
 typedef struct lf_node {
     int data;
-    lf_node *next;
     lf_node *prev;
 } lf_node;
 
@@ -28,14 +27,13 @@ lf_queue_t *lf_create_queue() {
 }
 
 void lf_destroy_queue(lf_queue_t *queue) {
-    // Assumes the thread is not being used anymore
-    lf_node *current = queue->back;
-    lf_node *next = NULL;
+    lf_node *current = queue->front;
+    lf_node *prev = NULL;
 
     while (current != NULL) {
-        next = current->next;
+        prev = current->prev;
         free(current);
-        current = next;
+        current = prev;
     }
     free(queue);
 }
@@ -45,14 +43,14 @@ void lf_print_queue(lf_queue_t *queue) {
     if (lf_is_empty(queue)) {
         printf("Queue is empty\n");
     }
-    lf_node *current = queue->back;
+    lf_node *current = queue->front;
     while (current != NULL) {
         printf("%d", current->data);
         
-        current = current->next;
+        current = current->prev;
         
         if (current != NULL) {
-            printf("->");
+            printf("<-");
         }
     }
     putchar('\n');
@@ -64,10 +62,6 @@ bool lf_is_empty(lf_queue_t *queue) {
 }
 
 int lf_enqueue(lf_queue_t *queue, int data) {
-    // put old_back->prev = new_node
-    // Put queue->back = new_node
-    // new_node->next = old_back
-
     lf_node *new_node = malloc(sizeof(lf_node));
     if (new_node == NULL) {
         fprintf(stderr, "Error allocating new queue node");
@@ -75,7 +69,6 @@ int lf_enqueue(lf_queue_t *queue, int data) {
     }
     new_node->data = data;
     new_node->prev = NULL;
-    new_node->next = NULL;
 
     lf_node *old_back = NULL;
 
@@ -86,9 +79,9 @@ int lf_enqueue(lf_queue_t *queue, int data) {
     
     do { // queue->back is not NULL
         old_back = queue->back;
-        new_node->next = old_back;
-    } while(!__sync_bool_compare_and_swap(&queue->back->prev, NULL, new_node));
-    atomic_store(&queue->back, new_node);
+    // } while(!__sync_bool_compare_and_swap(&queue->back->prev, NULL, new_node));
+    } while(!__sync_bool_compare_and_swap(&queue->back, old_back, new_node));
+    atomic_store(&old_back->prev, new_node);
     return 0;
 }
 
@@ -104,13 +97,10 @@ int lf_dequeue(lf_queue_t *queue, int *data) {
         *data = old_front->data;
     } while (!__sync_bool_compare_and_swap(&queue->front, old_front, old_front->prev));
     
-    if (!__sync_bool_compare_and_swap(&queue->back, old_front, NULL)) { // if queue not empty
-        atomic_store(&queue->front->next, NULL);
+    if (!__sync_bool_compare_and_swap(&queue->back, old_front, NULL)) { // if queue still not empty
+        atomic_store(&old_front->prev, NULL);
     }
     
     free(old_front);
     return 0;
 }
-
-
-
